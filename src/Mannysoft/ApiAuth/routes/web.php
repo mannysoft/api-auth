fc4d8d737030828c23a89cf70d71b7943a6c9f03
Manny Isles <mannysoft@gmail.com>
2018-06-06 11:50:36 +0800

    Update web.php

--- src/Mannysoft/ApiAuth/routes/web.php
+++ src/Mannysoft/ApiAuth/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+Route::post(config('api-auth.register'), 'Mannysoft\ApiAuth\Controllers\AuthenticateController@register');
 Route::post(config('api-auth.login_url'), 'Mannysoft\ApiAuth\Controllers\AuthenticateController@authenticate');
 Route::post(config('api-auth.logout_url'), 'Mannysoft\ApiAuth\Controllers\AuthenticateController@logout')->middleware('auth:api');
 Route::post(config('api-auth.login_facebook'), 'Mannysoft\ApiAuth\Controllers\AuthenticateController@loginFacebook');
