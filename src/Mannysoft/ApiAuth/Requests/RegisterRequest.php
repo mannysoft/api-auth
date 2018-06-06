7b0fc067c0d9be1e36d2e1eb94e01f24bb3ce633
Manny Isles <mannysoft@gmail.com>
2018-06-06 11:50:11 +0800

    Create RegisterRequest.php

---
+++ src/Mannysoft/ApiAuth/Requests/RegisterRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Mannysoft\ApiAuth\Requests;
+
+use Mannysoft\ApiFormRequest\ApiFormRequest;
+
+class RegisterRequest extends ApiFormRequest
+{
+    // Whether you want to use other status code other than 422.
+    // You can use 400 also
+    protected $statusCode = 422;
+    
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+        ];
+    }
+}
